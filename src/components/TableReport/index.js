c5d2e5384ed2178c67eb71b0693fc85fa9fed45c
Max <maxsoncoelho@gmail.com>
2022-04-08 15:59:59 -0400

    search report response api ok

---
+++ src/components/TableReport/index.js
@@ -0,0 +1,97 @@
+import React, { useState, useContext } from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { AuthContext } from '../../contexts/auth';
+import './TableReport.css';
+
+
+export default function StickyHeadTable({ dataBactery }) {
+  const { token } = useContext(AuthContext);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const columns = [
+    { id: 'codigo', label: 'Código', minWidth: 130 },
+    { id: 'cor', label: 'Cor', minWidth: 130 },
+    { id: 'forma', label: 'Forma', minWidth: 130 },
+    { id: 'elevacao', label: 'Elevação', minWidth: 130 },
+    { id: 'borda', label: 'Borda', minWidth: 130 },
+    { id: 'superficie', label: 'Superfície', minWidth: 130 }
+  ];
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dataBactery
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 25, 100]}
+        component="div"
+        count={dataBactery.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+
+
+
+
+
+
+
+
--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,69 +1,152 @@
 import React, { useState, useEffect, useContext } from 'react';
 import './report.css';
-import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
-import TableBactery from '../../components/TableBactery';
+import TableReport from '../../components/TableReport';
 import Button from '@mui/material/Button';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import api from '../../services/api';
 import { AuthContext } from '../../contexts/auth';
 import CircularIndeterminate from '../../components/CircularProgress';
-import { MicroContext } from '../../contexts/microorganismos';
+import PrintIcon from '@mui/icons-material/Print';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import htmlToPdfmake from "html-to-pdfmake"
 
 
 export default function Report() {
   const { token } = useContext(AuthContext);
-  const { setActiveDeletePhotos } = useContext(MicroContext);
   const [title, setTitle] = useState('RELATÓRIOS');
   const [visible, setVisible] = useState(true);
   const [offSearch, setOffSearch] = useState(false);
   const [dataBactery, setDataBactery] = React.useState([]);
   const [loading, setLoading] = useState(false);
+  const [valueSelection, setValueSelection] = useState('');
+  const [identMolecular, setIdentMolecular] = useState('');
+  const [pigment, setPigment] = useState('');
+  const [color, setColor] = useState('');
+  const [property, setProperty] = useState('');
+  const [dataReport, setDataReport] = useState([]);
 
-  async function getBactery(){
-    setLoading(true);
-    try{
-      const response = await api.get('/bacterias',{
-        headers:{
-          'Content-type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-          'Accept': '*/*'
+  async function searchReport() {
+
+    const response = await api.get(`/reports/${valueSelection}?identMolecular=${identMolecular}&cor=${color}&pigmento=${pigment}&propriedades=${property}`)
+    const res = response.data;
+    console.log(res);
+  }
+
+  function clientePDF(item){
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    const reportTitle = [
+      {
+        text: 'Centro de Biotécnologia da Amazônia',
+        fontSize: 20,
+        bold: true,
+        margin: [135, 20, 0, 30] // left, top, right, bottom
+      }
+    ];
+    const html = htmlToPdfmake(
+      ` <h5>Microorganismo Bacteriano</h5>
+      <table width="100%" border="1"  cellpadding="0" cellspacing="0">
+      <tr>
+         <td width="11%" >
+            <table><font  size="1">
+                 <tr><td><strong>Código: ${item.codigo}</strong></td></tr>
+                 <tr><td><strong>Identificação Molecular: ${item.identMolecular == undefined ? 'Não especificado' : item.identMolecular}</strong></td></tr>
+                 <tr><td><strong>Cor: ${item.cor == undefined ? 'Não especificado' : item.cor}</strong></td></tr>
+                 <tr><td><strong>Forma: ${item.forma  == undefined ? 'Não especificado' : item.forma}</strong></td></tr>
+                 <tr><td><strong>Elevação: ${item.elevacao == undefined ? 'Não especificado' : item.elevacao}</strong></td></tr>
+                 <tr><td><strong>Tipo de Crescimento: ${item.tipoCresc == undefined ? 'Não especificado' : item.tipoCresc}</strong></td></tr>
+                 <tr><td><strong>Borda: ${item.borda == undefined ? 'Não especificado' : item.borda}</strong></td></tr>
+                 <tr><td><strong>Esporula: ${item.esporula == undefined ? 'Não especificado' : item.esporula}</strong></td></tr>
+                 <tr><td><strong>Superfície: ${item.superficie == undefined ? 'Não especificado' : item.superficie}</strong></td></tr>
+                 <tr><td><strong>Consistência: ${item.consistencia == undefined ? 'Não especificado' : item.consistencia}</strong></td></tr>
+                 <tr><td><strong>Detalhes Ópticos: ${item.detOpticos == undefined ? 'Não especificado' : item.detOpticos}</strong></td></tr>
+                 <tr><td><strong>Pigmento: ${item.pigmento == undefined ? 'Não especificado' : item.pigmento}</strong></td></tr>
+                 <tr><td><strong>Propriedades: ${item.propriedades == undefined ? 'Não especificado' : item.propriedades}</strong></td></tr>
+                 <tr><td><strong>Meio de Isolamento: ${item.meioIsolamento == undefined ? 'Não especificado' : item.meioIsolamento}</strong></td></tr>
+                 <tr><td><strong>Tempo de Incubação: ${item.tempIncubacao == undefined ? 'Não especificado' : item.tempIncubacao}</strong></td></tr>
+                 <tr><td><strong>Descrição do Isolado: ${item.descricaoIsolado == undefined ? 'Não especificado' : item.descricaoIsolado}</strong></td></tr>
+                 <tr><td><strong>Data de Coleta: ${item.dataColeta == undefined ? 'Não especificado' : item.dataColeta}</strong></td></tr>
+                 <tr><td><strong>Local de Coleta: ${item.localColeta == undefined ? 'Não especificado' : item.localColeta}</strong></td></tr>
+                 <tr><td><strong>Data de Reativação: ${item.dataReativacao == undefined ? 'Não especificado' : item.dataReativacao}</strong></td></tr>
+                 </tr></font>
+             </table>
+        </td>
+      </tr>
+   </table>
+   `
+    )
+    function rodape(currentPage, pageCount){
+      return [
+        {
+          text: currentPage + ' / ' + pageCount,
+          alignment: 'right',
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 20, 0] // left, top, right, bottom
         }
-      });
-  
-      const result = response.data;
-      setDataBactery(result.reverse());
-      setLoading(false);
+      ]
     }
-    catch(error){
-      setLoading(false);
-      alert('Erro na listagem de coleções bacterianas, verifique sua conexão com a internet.')
-      console.log(error);
+    const docDefinitions = {
+      pageSize: 'A4',
+      pageMargins: [15, 50, 15, 40],
+      header: [reportTitle],
+      content: [html],
+      footer: rodape
     }
+
+    pdfMake.createPdf(docDefinitions).download();
   }
 
   useEffect(()=> {
-    setActiveDeletePhotos(true);
-    getBactery();
+    
   }, [])
 
   return (
     <div className="containerAll">
-      <Header title={title} visible={visible} offSearch={offSearch} setDataBactery={setDataBactery}/>
-      <div className='containerTableBactery'>
-        
+      <Header title={title} visible={visible} offSearch={offSearch} />
+      <div className="areaSearch">
+        <div className='smallAreaSelection'>
+            <span>Selecione o tipo de relatório:</span>
+            <select value={valueSelection} name="selection" id="input" onChange={e=> setValueSelection(e.target.value)}>
+                <option value="">Selecione</option>
+                <option value='bacterias'>Bactérias</option>
+                <option value='fungos'>Fungos</option>
+            </select>
+        </div>
+          <div style={{flexDirection: 'column', width: 150, marginRight: 10}}>
+            <span>Ident Molecular:</span>
+            <input type="text" placeholder="digite" placeholderTextColor="#ccc" id="inputSearch" onChange={e=> setIdentMolecular(e.target.value)}/>
+          </div>
+          <div style={{flexDirection: 'column', width: 150, marginRight: 10}}>
+            <span>Pigmento:</span>
+            <input type="text" placeholder="digite" placeholderTextColor="#ccc" id="inputSearch" onChange={e=> setPigment(e.target.value)}/>
+          </div>
+          <div style={{flexDirection: 'column', width: 150, marginRight: 10}}>
+            <span>Cor:</span>
+            <input type="text" placeholder="digite" placeholderTextColor="#ccc" id="inputSearch" onChange={e=> setColor(e.target.value)}/>
+          </div>
+          <div style={{flexDirection: 'column', width: 150, marginRight: 10}}>
+            <span>Propriedade:</span>
+            <input type="text" placeholder="digite" placeholderTextColor="#ccc" id="inputSearch" onChange={e=> setProperty(e.target.value)}/>
+          </div>
+      </div>
+      <div style={{alignSelf: 'center', marginTop: 10}}>
+        <Button variant="contained">
+          <ManageSearchIcon onClick={searchReport} />
+        </Button>
+      </div>
+      <div className='containerTableBacteryReport'>
       {loading ?
         <CircularIndeterminate />
         :
-        <TableBactery dataBactery={dataBactery} getBactery={getBactery}/>
+        <TableReport dataBactery={dataBactery} />
       }
       </div>
-      <div className='areaButton'>
-        <Link to='/adicao-bacteriana'>
-          <Button id="btnAdd" variant="contained"> 
-            <AddCircleIcon />
-          </Button>
-        </Link>
+      <div style={{alignSelf: 'center', marginTop: 10}} className='areaButton'>
+        <Button variant="contained"> 
+          <PrintIcon onClick={clientePDF}  />
+        </Button>
       </div>
     </div>
   )
